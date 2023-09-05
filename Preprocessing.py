38556c516966365effac64d2f19f739b446f1a18
bomi <bombom713@naver.com>
2023-09-05 23:33:21 +0900

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -5,6 +5,23 @@ import json
 from PIL import Image
 import torchvision.transforms as transforms
 
+def Ip_Ic(image_path):
+    image = Image.open(image_path).convert('RGB')
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    return transform(image)
+
+image_path_person = './Model-Image/1008_A001_000.jpg'
+image_path_garment = './Item-Image/1008002_F.jpg'
+
+# 이미지 로드
+Ip = Ip_Ic(image_path_person)
+Ic = Ip_Ic(image_path_garment)
+
+# print(Ip)
+# print(Ic)
+
 def sp_sg(data):
     segmentations = []
     if isinstance(data, list):
@@ -61,41 +78,28 @@ Jg = jp_jg(data_jg)[0]  # garment pose keypoints
 # print(Jp)
 # print(Jg)
 
-def Ip_Ic(image_path):
-    image = Image.open(image_path).convert('RGB')
-    transform = transforms.Compose([
-        transforms.ToTensor()
-    ])
-    return transform(image)
+def Ia(Ip, Sp, Jp):
+    # Generate masks for head, hands, and lower body based on Sp and Jp
+    # For simplicity, we're returning dummy masks. This needs to be implemented properly.
+    head_mask = torch.zeros_like(Sp)
+    hands_mask = torch.zeros_like(Sp)
+    lower_body_mask = torch.zeros_like(Sp)
 
-image_path_person = './Model-Image/1008_A001_000.jpg'
-image_path_garment = './Item-Image/1008002_F.jpg'
-
-# 이미지 로드
-Ip = Ip_Ic(image_path_person)
-Ic = Ip_Ic(image_path_garment)
-
-# print(Ip)
-# print(Ic)
-
-def agnostic RGB(Ip, Sp, Jp):
     masked_person = Ip * (1 - Sp)
-
-    head_mask, hands_mask, lower_body_mask = generate_masks(Sp, Jp)
     clothing_agnostic = masked_person + Ip * head_mask + Ip * hands_mask + Ip * lower_body_mask
 
     return clothing_agnostic
+
+# Normalize pose keypoints to the range of [0, 1]
+Jp = (Jp - Jp.min()) / (Jp.max() - Jp.min())
+Jg = (Jg - Jg.min()) / (Jg.max() - Jg.min())
 
 # Assuming Ip and Ic are given
 Sp = F.interpolate(Sp.unsqueeze(0).unsqueeze(0), size=(Ip.size(1), Ip.size(2)), mode='bilinear', align_corners=False).squeeze(0).squeeze(0)
 Sg = F.interpolate(Sg.unsqueeze(0).unsqueeze(0), size=(Ic.size(1), Ic.size(2)), mode='bilinear', align_corners=False).squeeze(0).squeeze(0)
 
-Ia = agnostic RGB(Ip, Sp, Jp)
+Ia = Ia(Ip, Sp, Jp)
 Ic = Ic * Sg  # Segment out the garment using the parsing map
-
-# Normalize pose keypoints to the range of [0, 1]
-Jp = (Jp - Jp.min()) / (Jp.max() - Jp.min())
-Jg = (Jg - Jg.min()) / (Jg.max() - Jg.min())
 
 ctryon = (Ia, Jp, Ic, Jg)
 
